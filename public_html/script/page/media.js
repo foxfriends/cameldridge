1eb941bc3a0a0073cf4ea5f51d076aa634af5e75
Cameron Eldridge <cameron_e@rogers.com>
2016-09-01 14:41:28 -0400

    Add last of content (websites)

---
+++ public_html/script/page/media.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export const phone = window.matchMedia('(max-width: 640px)');
+export const tablet = window.matchMedia('(min-width: 641px) and (max-width: 960px)');
+export const desktop = window.matchMedia('(min-width: 961px)');

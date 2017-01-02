5bc1a7c611f01b0ee2f9694b5c11ed013a8b1799
Cameron Eldridge <cameldridge+git@gmail.com>
2017-01-02 17:57:20 -0500

    webpack code splitting for games module

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 'use strict';
 const webpack = require('webpack');
 
-module.exports = [{
-  entry: './public_html/script/index.cp.js',
-  output: {
-    path: './public_html',
-    filename: 'index.cp.min.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /(node_modules).*\.js/, loader: 'babel-loader?presets[]=es2015' },
-      { test: /\.s(a|c)ss$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'style-loader!css-loader!sass-loader' },
-      { test: /\.(gif|svg|png|jpe?g)$/, loader: 'file-loader?name=images/[hash].[ext]!img-loader' }
-    ]
-  }
-}, {
-  entry: './public_html/script/index.js',
-  output: {
-    path: './public_html',
-    filename: 'index.min.js'
-  },
-  module: {
-    loaders: [
-      // { test: /\.js$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'babel-loader?plugins[]=transform-es2015-modules-commonjs' },
-      { test: /\.s(a|c)ss$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'style-loader!css-loader!sass-loader' },
-      { test: /\.(gif|svg|png|jpe?g)$/, loader: 'file-loader?name=images/[hash].[ext]!img-loader' }
-    ]
-  }
-}];
+module.exports = function() {
+  return [{
+    entry: './public_html/script/index.cp.js',
+    output: {
+      path: './public_html',
+      filename: 'index.cp.min.js'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /(node_modules).*\.js/, loader: 'babel-loader?presets[]=es2015' },
+        { test: /\.s(a|c)ss$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'style-loader!css-loader!sass-loader' },
+        { test: /\.(gif|svg|png|jpe?g)$/, loader: 'file-loader?name=images/[hash].[ext]!img-loader' }
+      ]
+    }
+  }, {
+    entry: './public_html/script/index.js',
+    output: {
+      path: './public_html',
+      filename: 'index.min.js'
+    },
+    module: {
+      rules: [
+        // { test: /\.js$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'babel-loader?plugins[]=transform-es2015-modules-commonjs' },
+        { test: /\.s(a|c)ss$/, exclude: /(node_modules|js-modules).*\.js/, loader: 'style-loader!css-loader!sass-loader' },
+        { test: /\.(gif|svg|png|jpe?g)$/, loader: 'file-loader?name=images/[hash].[ext]!img-loader' }
+      ]
+    }
+  }];
+};

672eccb9f2593560af4c8fe3fca517286b792ae7
Cameron Eldridge <cameldridge+git@gmail.com>
2017-07-29 15:50:07 -0400

    Minify files, stop supporting IE

--- script/main/game-engine/util.js
+++ script/main/game-engine/util.js
@@ -9,69 +9,100 @@ const [
   TO_POSITION
 ] = Symbolic;
 
-export default function Util(game) {
-  let [method, target, flags, at] = ['', null, new Set(), null];
-  const util = function(...args) { return method(...args); };
-  Object.defineProperties(util, {
-    to: { get() { return util; } },
-    and: { get() { return util; } },
-    distance: { get() { return method = distance, util; } },
-    direction: { get() { return method = direction, util; } },
-    random: { get() { return method = random, util; } },
-    collision: { get() { return method = collision, util; } },
-    integer: { get() { return flags.add('integer'), util; } },
-    at: { value(where) { return at = where, flags.add('at'), util; } },
-    between: { value(what) { return target = what, util; } },
-    from: { value(what) { return target = what, util; } }
-  });
-
-  function toPosition(inst) {
-    switch(typeof inst) {
-      case 'function':
-        inst = game.actors.find((x) => x instanceof inst).position;
-        break;
-      case 'object':
-        if(inst.position) {
-          inst = inst.position;
-        }
-        break;
-    }
-    return inst;
+export default class Util extends Function {
+  constructor(game) {
+    super('...args', `return this.execute(...args);`);
+    this[GAME] = game;
+    this[METHOD] = '';
+    this[TARGET] = null;
+    this[FLAGS] = new Set();
   }
 
-  function distance(inst) {
-    target = toPosition(target);
-    inst = toPosition(inst);
-    return Math.sqrt(Math.pow(target[0] - inst[0], 2) + Math.pow(target[1] - inst[1], 2));
+  get to() {
+    return this;
+  }
+  get and() {
+    return this;
   }
 
-  function direction(inst) {
-    target = toPosition(target);
-    inst = toPosition(inst);
-    return Math.atan2(inst[1] - target[1], inst[0] - target[0]);
+  get distance() {
+    this[METHOD] = DISTANCE;
+    return this;
   }
 
-  function random(max) {
-    const number = Math.random() * (max - target) + target;
-    if(flags.has('integer')) {
+  get direction() {
+    this[METHOD] = DIRECTION;
+    return this;
+  }
+
+  get random() {
+    this[METHOD] = RANDOM;
+    return this;
+  }
+
+  get collision() {
+    this[METHOD] = COLLISION;
+    return this;
+  }
+
+  get integer() {
+    this[FLAGS].add('integer');
+    return this;
+  }
+
+  at(pos) {
+    this[FLAGS].add('at');
+    this[AT] = this[TO_POSITION](pos);
+    return this;
+  }
+
+  between(what) {
+    this[TARGET] = what;
+    return this;
+  }
+
+  from(what) {
+    this[TARGET] = what;
+    return this;
+  }
+
+  execute(...args) {
+    return this[this[METHOD]](...args);
+  }
+
+  [DISTANCE](inst) {
+    this[TARGET] = this[TO_POSITION](this[TARGET]);
+    inst = this[TO_POSITION](inst);
+    return Math.sqrt(Math.pow(this[TARGET][0] - inst[0], 2) + Math.pow(this[TARGET][1] - inst[1], 2));
+  }
+
+  [DIRECTION](inst) {
+    this[TARGET] = this[TO_POSITION](this[TARGET]);
+    inst = this[TO_POSITION](inst);
+    return Math.atan2(inst[1] - this[TARGET][1], inst[0] - this[TARGET][0]);
+  }
+
+  [RANDOM](max) {
+    const number = Math.random() * (max - this[TARGET]) + this[TARGET];
+    if(this[FLAGS].has('integer')) {
       return Math.floor(number);
     }
     return number;
   }
 
-  function collision(what) {
+  [COLLISION](what) {
     const targets = [[], []];
-    switch(typeof target) {
+    switch(typeof this[TARGET]) {
       case 'function':
-        targets[0] = game.actors.filter((a) => a instanceof target);
+        targets[0] = this[GAME].actors.filter((a) => a instanceof this[TARGET]);
         break;
       case 'object':
-        targets[0].push(target);
+        targets[0].push(this[TARGET]);
         break;
     }
     switch(typeof what) {
       case 'function':
-        targets[1] = game.actors.filter((a) => a instanceof what);
+        targets[1] = this[GAME].actors.filter((a) => a instanceof what);
         break;
       case 'object':
         targets[1].push(what);
@@ -81,8 +112,8 @@ export default function Util(game) {
       for(let b of targets[1]) {
         let { position: pos_a, origin: ori_a, dimensions: dim_a } = a;
         let { position: pos_b, origin: ori_b, dimensions: dim_b } = b;
-        if(flags.has('at') && at) {
-          pos_a = at;
+        if(this[FLAGS].has('at') && this[AT]) {
+          pos_a = this[AT];
         }
         pos_a = [pos_a[0] - ori_a[0], pos_a[1] - ori_a[1]];
         pos_b = [pos_b[0] - ori_b[0], pos_b[1] - ori_b[1]];
@@ -96,5 +127,17 @@ export default function Util(game) {
     return false;
   }
 
-  return util;
+  [TO_POSITION](inst) {
+    switch(typeof inst) {
+      case 'function':
+        inst = this[GAME].actors.find((x) => x instanceof inst).position;
+        break;
+      case 'object':
+        if(inst.position) {
+          inst = inst.position;
+        }
+        break;
+    }
+    return inst;
+  }
 }

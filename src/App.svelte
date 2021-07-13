e434886eee00c2832addd46ee429d5a352025446
Cameron Eldridge <cameldridge@gmail.com>
2021-07-13 08:54:04 -0400

    Remove the css images and fonts, as they are now moved

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,18 @@
 <script>
-  import ImageProvider from 'scattered-papers/ImageProvider.svelte';
   import DesktopSite from './DesktopSite.svelte';
   import MobileSite from './MobileSite.svelte';
-  const images = Object
-    .fromEntries(Object
-      .entries(import.meta.globEager('./image/**/*.{png,svg}'))
-      .map(([path, value]) => [path.slice('./image/'.length), value]));
 
   let width, height, mouseX, mouseY, scroll, contentHeight;
 </script>
 
-<ImageProvider {images}>
-  {#if width > 700}
-    <div class='noscroll'>
-      <DesktopSite {width} {height} {contentHeight} {mouseX} {mouseY} {scroll} />
-    </div>
-    <div class='scroll' style='height: 800vh' bind:clientHeight={contentHeight} />
-  {:else}
-    <MobileSite />
-  {/if}
-</ImageProvider>
+{#if width > 700}
+  <div class='noscroll'>
+    <DesktopSite {width} {height} {contentHeight} {mouseX} {mouseY} {scroll} />
+  </div>
+  <div class='scroll' style='height: 800vh' bind:clientHeight={contentHeight} />
+{:else}
+  <MobileSite />
+{/if}
 
 <svelte:body on:mousemove={e => { mouseX = e.clientX; mouseY = e.clientY; }} />
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} bind:scrollY={scroll} />
